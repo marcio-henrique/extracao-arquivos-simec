951327e4f416dd134090a2d11b1baf6c1fe37c07
Márcio Henrique <ikke.guerreiro@gmail.com>
2021-03-18 10:48:24 -0300

    Script de download dos dados do simec

---
+++ restrictions.py
@@ -0,0 +1,5 @@
+formatos_aceitos = ['Digital', 'MEC', 'Daisy', 'PDF', 'Audiovisual']
+formatos_nao_aceitos = ['Braille', 'Impresso']
+
+exemplares_aceitos = ['Livro do aluno', 'Manual do professor']
+exemplares_nao_aceitos = ['Material do gestor', 'Formação do professor']
